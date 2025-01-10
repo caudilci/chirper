362189892671317df59d530aed0e286b2b1631e9
Chris <christopheriancaudill@gmail.com>
2025-01-09 20:34:08 -0500

    pretty

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -17,14 +17,17 @@
                             d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                     </svg>
                     <div class="flex-1">
-                        <div class="flex justify-between items-center">
-                            <div>
+                        <div class="flex justify-between items-center flex-col">
+                            <div class="w-full flex flex-row justify-between">
                                 <span class="text-gray-800"> {{ $chirp->user->name }} </span>
-                                <small class="ml-2 text-sm text-gray-600"> {{ $chirp->created_at->format('j M Y, g:i a') }}
-                                </small>
-                                @unless ($chirp->created_at->eq($chirp->updated_at))
-                                    <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
-                                @endunless
+                                <div>
+                                    <small class="ml-2 text-sm text-gray-600">
+                                        {{ $chirp->created_at->format('j M Y, g:i a') }}
+                                    </small>
+                                    @unless ($chirp->created_at->eq($chirp->updated_at))
+                                        <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
+                                    @endunless
+                                </div>
                                 @if ($chirp->user->is(auth()->user()))
                                     <x-dropdown>
                                         <x-slot name="trigger">
@@ -52,7 +55,7 @@
                                     </x-dropdown>
                                 @endif
                             </div>
-                            <p class="mt-4 text-lg text-gray-900"> {{ $chirp->message }} </p>
+                            <p class="justify-self-start w-full mt-4 text-lg text-gray-900"> {{ $chirp->message }} </p>
                         </div>
                     </div>
                 </div>
